c93dbc01629f5863fcdd3835bcb442e5f15de5ca
Lucas <lucasp0927@gmail.com>
2010-08-23 01:26:27 +0800

    almost done

--- plurknotify.py
+++ plurknotify.py
@@ -5,12 +5,13 @@ import time
 import pynotify
 from datetime import datetime
 import json
+import os
 
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
 api_key = 'vB8TYzK9lyDFfHvCjSf0RlF9KBYAUTaL'
 get_api_url = lambda x: 'http://www.plurk.com/API%s' % x
 encode = urllib.urlencode
-
+fileurl = "file://" + os.path.abspath(os.path.curdir)+"/temp.png"
 file_line = open("password.dat","r").readlines()
 username = file_line[0].strip()
 password = file_line[1].strip()
@@ -47,17 +48,32 @@ while(True):
     friends = {}
     for uid in plurks['plurk_users']:
         if 'display_name' in plurks['plurk_users'][uid]:
-            friends[uid] = plurks['plurk_users'][uid]['display_name']
+            friends[uid] = {}
+            friends[uid]['name'] = plurks['plurk_users'][uid]['display_name']
+            if plurks['plurk_users'][uid]['has_profile_image'] == 1 and plurks['plurk_users'][uid]['avatar'] == None:
+                friends[uid]['url'] = 'http://avatars.plurk.com/'+str(uid)+'-small.gif'
+            elif plurks['plurk_users'][uid]['has_profile_image'] == 1 and plurks['plurk_users'][uid]['avatar'] != None:
+                friends[uid]['url'] = 'http://avatars.plurk.com/'+str(uid)+'-small'+str(plurks['plurk_users'][uid]['avatar'])+'.gif'
+            else:
+                friends[uid]['url'] = 'http://www.plurk.com/static/default_small.gif'
         else:
-            friends[uid] = plurks['plurk_users'][uid]['nick_name']
+            friends[uid] = {}
+            friends[uid]['name'] = plurks['plurk_users'][uid]['nick_name']
+            if plurks['plurk_users'][uid]['has_profile_image'] == 1 and plurks['plurk_users'][uid]['avatar'] == None:
+                friends[uid]['url'] = 'http://avatars.plurk.com/'+str(uid)+'-small.gif'
+            elif plurks['plurk_users'][uid]['has_profile_image'] == 1 and plurks['plurk_users'][uid]['avatar'] != None:
+                friends[uid]['url'] = 'http://avatars.plurk.com/'+str(uid)+'-small'+str(plurks['plurk_users'][uid]['avatar'])+'.gif'
+            else:
+                friends[uid]['url'] = 'http://www.plurk.com/static/default_small.gif'
     print friends
 
     for p in plurks['plurks']:
+#        urllib.urlretrieve(friends[str(p['owner_id'])]['url'], fileurl)
         if 'qualifier_translated' in p:
-            n = pynotify.Notification (friends[str(p['owner_id'])] + ' ' + p['qualifier_translated'],str(p['content']))
+            n = pynotify.Notification (friends[str(p['owner_id'])]['name'] + ' ' + p['qualifier_translated'],str(p['content']),fileurl)
         else:
-            n = pynotify.Notification (friends[str(p['owner_id'])] + ' ' + p['qualifier'],str(p['content']))
+            n = pynotify.Notification (friends[str(p['owner_id'])]['name'] + ' ' + p['qualifier'],str(p['content']),fileurl)
         n.show()
     print 'finish a cycle'
-    time.sleep(180)
+    time.sleep(60)
 
