6d8e9510db722169dbf548a8bb2fc5611077533f
lucaspeng <lucaspeng@netbook.localdomain>
2010-08-22 21:49:06 +0800

    add plurknotify.py

---
+++ plurknotify.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python
+#--- Setup ----------------------------------------------
+import urllib, urllib2, cookielib
+import time
+import pynotify
+from datetime import datetime
+import simplejson
+
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+api_key = 'vB8TYzK9lyDFfHvCjSf0RlF9KBYAUTaL'
+get_api_url = lambda x: 'http://www.plurk.com/API%s' % x
+encode = urllib.urlencode
+password = ''
+encode = urllib.urlencode
+
+username = 'lucasp0927'
+#read password
+passfile="password.dat"
+infile = open(passfile,"r")
+password = infile.readline()
+pynotify.init("plurk")
+
+offset = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
+print 'offset =',
+print offset
+while(True):
+    time.sleep(180)                            
+    
+    opener.open('https://www.plurk.com/API/Users/login',
+                     encode({'username': username,
+                             'password': password,
+                             'api_key': api_key}))
+    print time.strftime(ISOTIMEFORMAT,time.gmtime(time.time())) 
+    fp2 = opener.open(get_api_url('/Polling/getPlurks'),
+                     encode({'api_key': api_key,
+                             'offset': offset,
+                             'limit' : 5}))
+    offset = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
+    print 'offset =',
+    print offset
+    plurk = simplejson.load(fp2)
+    print plurk
+    print len(plurk['plurks'])
+    for p in plurk['plurks']:
+        n = pynotify.Notification ("New plurk from ",str(p['content']))
+        n.show()
+    print 'finish a cycle'
